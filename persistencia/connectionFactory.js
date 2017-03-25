618497f35c1ea447360241a5cc67a55e46bb8843
Guilherme Fiola <guilhermefiola@gmail.com>
2017-03-24 23:03:19 -0300

    Formatação

--- persistencia/connectionFactory.js
+++ persistencia/connectionFactory.js
@@ -1,12 +1,12 @@
 var mysql  = require('mysql');
 
 function createDBConnection(){
-return mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'admin',
-    database: 'payfast'
-});
+    return mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'admin',
+        database: 'payfast'
+    });
 }
 
 module.exports = function() {
